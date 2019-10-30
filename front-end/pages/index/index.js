9639697b2f3043dd2fbe1426dc585339b95d65f1
maglith <942399293@qq.com>
2019-10-30 23:29:39 +0800

    goodsList
    goodsList page

--- front-end/pages/index/index.js
+++ front-end/pages/index/index.js
@@ -8,7 +8,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     currentTab: 0,
-    reqArray: [{
+    reqList: [{
       title: "蔡徐坤",
       detail: "12月川大坤坤演唱会组队"
     },
@@ -23,6 +23,51 @@ Page({
     {
       title: "吴亦凡",
       detail: "you bad oh you bad"
+    }],
+    actList: [{
+      title: "新裤子乐队",
+      detail: "你你你你要跳舞吗"
+    },
+    {
+      title: "刺猬乐队",
+      detail: "黑色的不是夜晚，是漫长的孤单"
+    },
+    {
+      title: "Nirvana",
+      detail: "come, as you are, as you were"
+    },
+    {
+      title: "Oasis",
+      detail: "don't look back in anger"
+    },
+    {
+      title: "九连真人",
+      detail: "哦善良，终究被中伤，哦善良，终究被重赏"
+    }],
+    goodsList: [{
+      title: "Beats耳机",
+      price: "899",
+      detail: "九成新，八折出"
+    },
+    {
+      title: "考研政治书",
+      price: "35",
+      detail: "成功上岸"
+    },
+    {
+      title: "计算机专业书",
+      price: "60",
+      detail: "三本打包价"
+    },
+    {
+      title: "airpods二代",
+      price: "1000",
+      detail: "换新款"
+    },
+    {
+      title: "kindle",
+      price: "480",
+      detail: "9成新，到货2个月，很少碰，现寻有缘人blablabla"
     }]
   },
   //事件处理函数
@@ -81,7 +126,12 @@ Page({
   },
   checkReqDetail: function(e) {
     wx.navigateTo({
-      url: '../detail/detail?index=' + e.currentTarget.dataset.index,
+      url: '../detail/detail?type=req&index=' + e.currentTarget.dataset.index,
+    })
+  },
+  checkActDetail: function(e) {
+    wx.navigateTo({
+      url: '../detail/detail?type=act&index=' + e.currentTarget.dataset.index,
     })
   }
 })
